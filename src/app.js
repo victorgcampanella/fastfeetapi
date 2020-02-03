1375f9380cefc4d48799e12d098dcb8935b52599
victorgcampanella <vgcgalvao@outlook.com>
2020-02-03 20:30:39 -0300

    adicionando  inicial codes, sucrase and nodemon

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const routes = require('./routes')
+
+class App {
+  constructor(){
+    this.server = express()
+
+    this.middleswares()
+    this.routes()
+  }
+
+  middleswares(){
+    this.server.use(express.json())
+  }
+
+  routes(){
+    this.server.use(routes)
+  }
+}
+
+module.exports = new App().server
