99b1bffb48c14c3219b77bf7350b751baf0175c5
victorgcampanella <vgcgalvao@outlook.com>
2020-02-07 12:50:34 -0300

    adicionando hash no password

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import User from './app/models/User';
+// import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
@@ -7,13 +9,15 @@ const routes = new Router();
 //   response.json({ message: 'Hello FastFeet' })
 // );
 
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    name: 'Victor',
-    email: 'victor@gmail.com',
-    password_hash: '123456',
-  });
-  return response.json(user);
-});
+// routes.get('/', async (request, response) => {
+//   const user = await User.create({
+//     name: 'Victor',
+//     email: 'victor@gmail.com',
+//     password_hash: '123456',
+//   });
+//   return response.json(user);
+// });
+
+routes.post('/users', UserController.store);
 
 export default routes;
