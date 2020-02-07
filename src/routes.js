2f9d20b281a61ee1246f513424a5043f561dfd14
victorgcampanella <vgcgalvao@outlook.com>
2020-02-07 12:19:43 -0300

    criando model e conexao no banco

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello FastFeet' })
-);
+// routes.get('/', (request, response) =>
+//   response.json({ message: 'Hello FastFeet' })
+// );
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    name: 'Victor',
+    email: 'victor@gmail.com',
+    password_hash: '123456',
+  });
+  return response.json(user);
+});
 
 export default routes;
